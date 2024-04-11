4bfbee9bb62e54261c9e6890fcf243454f23aa4b
unknown <emaneldeeb1999@gmail.com>
2024-04-11 04:36:16 +0200

    final touches

--- Redux-Bank/src/features/Custmor/customrSlice.js
+++ Redux-Bank/src/features/Custmor/customrSlice.js
@@ -1,22 +1,47 @@
+// const initialCustmorState = {
+//   name: "",
+//   nationalid: "",
+// };
+
+// export default function custmorReducer(state = initialCustmorState, action) {
+//   switch (action.type) {
+//     case "customer/create":
+//       return {
+//         ...state,
+//         name: action.payload.name,
+//         nationalid: action.payload.nationalid,
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
+
+// export function actionCreateUser(name, id) {
+//   return { type: "customer/create", payload: { name: name, nationalid: id } };
+// }
+
+// redux toolkit
+import { createSlice } from "@reduxjs/toolkit";
 const initialCustmorState = {
   name: "",
   nationalid: "",
 };
+const customerSlice = createSlice({
+  name: "customer",
+  initialState: initialCustmorState,
+  reducers: {
+    createUser: {
+      prepare(fullName, nationalId) {
+        return { payload: { fullName, nationalId } };
+      },
+      reducer(state, action) {
+        state.name = action.payload.fullName;
+        state.nationalid = action.payload.nationalId;
+      },
+    },
+  },
+});
 
-export default function custmorReducer(state = initialCustmorState, action) {
-  switch (action.type) {
-    case "customer/create":
-      return {
-        ...state,
-        name: action.payload.name,
-        nationalid: action.payload.nationalid,
-      };
-
-    default:
-      return state;
-  }
-}
-
-export function actionCreateUser(name, id) {
-  return { type: "customer/create", payload: { name: name, nationalid: id } };
-}
+export default customerSlice.reducer;
+export const { createUser } = customerSlice.actions;
