64111f8073a520294d344e8f55509a5ea1c51c23
unknown <emaneldeeb1999@gmail.com>
2024-04-18 05:15:08 +0200

    react-router and user functionaltiy

---
+++ PizzaCO/src/redux/UserSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+  userName: '',
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    createUser(state, action) {
+      state.userName = action.payload;
+    },
+  },
+});
+
+export const { createUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
